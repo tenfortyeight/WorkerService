1b63fdb2e41e272cef5a8043251f9f0bce3e28a9
unknown <joma4@WRKSEPLTJOMA4.Betsson.local>
2013-11-13 14:29:36 +0100

    cleaned up some unused crap

--- Worker/Program.cs
+++ Worker/Program.cs
@@ -18,52 +18,6 @@ namespace Worker
 			{
 				proxy.Invoke("Send", name, message).Wait();
 			}
-
-
-
-
-			//const string name = "Console";
-			//var communicator = MessageCommunicator.Instance;
-
-			//while (true)
-			//{
-			//	Console.Write("Message: ");
-			//	var message = Console.ReadLine();
-
-			//	communicator.SendMessage(name, message);
-			//}
 		}
 	}
-
-	//public class MessageCommunicator : IMessageCommunicator
-	//{
-	//	private readonly IHubContext _hubContext;
-	//	private static IMessageCommunicator _instance;
-
-	//	private MessageCommunicator(IHubContext hubContext)
-	//	{
-	//		if (hubContext == null) throw new ArgumentNullException("hubContext");
-			
-	//		_hubContext = hubContext;
-	//	}
-
-	//	public static IMessageCommunicator Instance
-	//	{
-	//		get
-	//		{
-	//			return _instance ?? new MessageCommunicator(HubContextProvider.GetHubContext<ChatHub>());
-	//		}
-	//		set { _instance = value; }
-	//	}
-
-	//	public void SendMessage(string name, string message)
-	//	{
-	//		_hubContext.Clients.All.nisse(name, message);
-	//	}
-	//}
-
-	//public interface IMessageCommunicator
-	//{
-	//	void SendMessage(string name, string message);
-	//}
 }
